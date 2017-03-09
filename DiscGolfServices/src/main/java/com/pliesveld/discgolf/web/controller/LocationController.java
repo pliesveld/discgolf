5be2130f4817861f003ba205b7d5907c130e5b35
Patrick Liesveld <pliesveld@users.noreply.github.com>
2017-03-08 19:27:01 -0500

    Location unit test

---
+++ DiscGolfServices/src/main/java/com/pliesveld/discgolf/web/controller/LocationController.java
@@ -0,0 +1,23 @@
+package com.pliesveld.discgolf.web.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pliesveld.discgolf.domain.Location;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@RestController
+@RequestMapping("/location")
+public class LocationController {
+    final static private Logger LOG = LogManager.getLogger();
+
+    @PostMapping
+    public ResponseEntity<?> handleLocation(@RequestBody Location location) {
+        LOG.debug("Location = {}", location);
+        return ResponseEntity.ok(location);
+    }
+}
---
+++ DiscGolfServices/src/test/java/com/pliesveld/discgolf/domain/LocationTest.java
@@ -0,0 +1,41 @@
+package com.pliesveld.discgolf.domain;
+
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class LocationTest {
+    final static private Logger LOG = LogManager.getLogger();
+
+    static abstract class GeoJsonPointMixin {
+        GeoJsonPointMixin(@JsonProperty("longitude") double x, @JsonProperty("latitude") double y) {}
+    }
+
+    private ObjectMapper objectMapper = newObjectMapper();
+
+    private static ObjectMapper newObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.addMixIn(GeoJsonPoint.class, GeoJsonPointMixin.class);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return objectMapper;
+    }
+
+    @Test
+    public void givenLocationJson_whenDeserialize_shouldConvertToObject() throws Exception {
+        String locationStr = "{ \"location\": {\"latitude\":40.759686, \"longitude\":-73.985235} }";
+        Location location = objectMapper.readValue(locationStr, Location.class);
+        assertNotNull(location);
+        assertNotNull(location.getLocation());
+        LOG.debug(location);
+    }
+}
