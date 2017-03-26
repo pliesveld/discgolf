307e42f956102646a18b1a25a1c44ee96e38761d
Patrick Liesveld <pliesveld@users.noreply.github.com>
2017-03-26 12:55:11 -0400

    resolved mongos usage of hibernate validator

--- DiscGolfServices/src/main/java/com/pliesveld/discgolf/config/MongoConfig.java
+++ DiscGolfServices/src/main/java/com/pliesveld/discgolf/config/MongoConfig.java
@@ -1,52 +1,26 @@
 package com.pliesveld.discgolf.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.core.geo.GeoJsonModule;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import com.fasterxml.jackson.databind.Module;
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
 import com.pliesveld.discgolf.repository.PlayerRepository;
 
 @Configuration
 @EnableMongoRepositories(basePackageClasses = PlayerRepository.class)
 //@EnableMongoAuditing(auditorAwareRef = "springSecurityAuditorAware")
-@Profile("PROD")
-public class MongoConfig extends AbstractMongoConfiguration {
-
-    @Value("${spring.data.mongodb.database}")
-    private String database;
-
-    @Value("${spring.data.mongodb.uri}")
-    private String uri;
-
-    @Bean
-    @Override
-    public Mongo mongo() throws Exception {
-        MongoClientURI mongoClientURI = new MongoClientURI(uri);
-        return new MongoClient(mongoClientURI);
-    }
-
-    @Override
-    protected String getDatabaseName() { return database; }
+public class MongoConfig {
 
     @Bean
     public Module registerGeoJsonModule(){ return new GeoJsonModule(); }
 
-//    @Bean
-//    public ValidatingMongoEventListener validatingMongoEventListener() { return new ValidatingMongoEventListener(validator()); }
-////    @Bean
-//    public LocalValidatorFactoryBean validator() {
-//        return new LocalValidatorFactoryBean();
-//    }
+    @Bean
+    @Autowired
+    public ValidatingMongoEventListener validatingMongoEventListener(javax.validation.Validator validator) { return new ValidatingMongoEventListener(validator); }
 
 }
 
